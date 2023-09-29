05a451c59fbd8d821e9d52f2d9632f4f6956f41c
mar-ver <marie.verdeil@gmail.com>
2023-09-29 16:45:01 +0200

    adding placeholder

--- _site/_static/js/projectopen.js
+++ _site/_static/js/projectopen.js
@@ -1,9 +1,9 @@
-var btn_more = document.getElementsByClassName("con-title")
+var btn_more = document.querySelectorAll(".con-contents, .con-title")
     //console.log(btn_more)
 for (var i = 0; i < btn_more.length; i++) {
     btn_more[i].addEventListener('click', function() {
         target = this.getAttribute('data-target')
-        //console.log(target)
+        console.log("click")
         document.getElementById(target).classList.toggle("open")
     })
 
