19999cef846583886d601eb0d02538d5a35dc2dd
mar-ver <marie.verdeil@gmail.com>
2023-10-09 09:44:24 +0200

    lsider

--- _site/_static/js/slider.js
+++ _site/_static/js/slider.js
@@ -25,49 +25,38 @@ var customizedFunction = function (info, eventName) {
     var info = slider.getInfo()
     var cur = info.index + 1 // get current slide starting from 1 
     var len = info.slideCount // total slides 
+    console.log(cur, len)
     if (cur>=len) {
+     
         setTimeout(() => {
        disableslider()
-          }, 1000);
-       
-        
-    }
+          }, 1200);
+        }   
+   
   }
 
 
-  //change anchor 
-function handleIntersection(entries) {
-  entries.forEach(({ isIntersecting } )=> {
-    console.log("sliderin view", sliderinview);
-   
-      if (isIntersecting) {
-      //anchor = target.getAttribute('title')
-      sliderinview = true
-      }
-      else {
-        sliderinview = false
-      }
 
-     
-  
-})
+  function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
 }
 
-const observer = new IntersectionObserver(handleIntersection, {
-  threshold: 0.5
-});
-document.querySelectorAll("#con-landing-slider").forEach(el => observer.observe(el));
 
-
-
-
+const landingslider = document.querySelector('#con-landing-slider');
 
 
   
 function disableslider(){
+  if(isInViewport(landingslider)){
     document.getElementById("description").scrollIntoView();
-    //slider.destroy();
-
+  }
 }
   // bind function to event
   slider.events.on('transitionEnd', customizedFunction);
